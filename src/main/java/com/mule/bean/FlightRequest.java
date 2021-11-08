24527c10d912b87b51660b84439420fb23faca2b
nitin <nitin.dharrao@cna.com>
2021-11-07 23:46:19 -0600

    API Contract changed to look jazzy - All APIs are Working

---
+++ src/main/java/com/mule/bean/FlightRequest.java
@@ -0,0 +1,28 @@
+package com.mule.bean;
+
+public class FlightRequest {
+
+	String airlineID;
+	String destination;
+	
+	public FlightRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAirlineID() {
+		return airlineID;
+	}
+
+	public void setAirlineID(String airlineID) {
+		this.airlineID = airlineID;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+}
